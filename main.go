13a0c15ec1fecbc9574b5453ad2bd8e142f08e1e
KaustubhD <kaustubhd287@gmail.com>
2019-10-16 23:24:24 +0530

    Adds GET and POST mechanism

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import(
 func main(){
 
   http.HandleFunc("/", homePage)
+  http.HandleFunc("/quotes", handleQuotes)
 
   fmt.Println("Server listening on port 8000")
   log.Panic(http.ListenAndServe(":8000", nil))
@@ -21,4 +22,22 @@ func homePage(writer http.ResponseWriter, request *http.Request){
     log.Panic(err)
   }
 
+}
+
+func handleQuotes(writer http.ResponseWriter, request *http.Request){
+  if request.Method == http.MethodPost{
+    postNewQuote(writer, request)
+  }else{
+    _, err := fmt.Fprintf(writer, "Only POST request available right now !")
+    if err != nil{
+      log.Panic(err)
+    }
+  }
+}
+
+func postNewQuote(writer http.ResponseWriter, request *http.Request){
+  _, err := fmt.Fprintf(writer, "Correct request")
+  if err != nil{
+    log.Panic(err)
+  }
 }
